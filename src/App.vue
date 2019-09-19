fd244b6ac79b99ae6013198c63a7573373bfb2f5
smallDreamm <1198134982@qq.com>
2019-09-19 15:14:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <headerNav></headerNav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import headerNav from'./components/header-nav'
+export default {
+  name: 'app',
+  components:{
+    headerNav
+  }
+}
+</script>
+
+<style>
+
+</style>
